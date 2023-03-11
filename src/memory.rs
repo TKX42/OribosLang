a27572cac90e513f6f5babfb4cf75a36ed33fc5a
TKX42 <tristan@kaupa.com>
2023-03-11 22:25:57 +0100

    big memory variable lookup performance optimization by using NoHashHasher

--- src/memory.rs
+++ src/memory.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
+use std::hash::BuildHasherDefault;
+
+use nohash_hasher::NoHashHasher;
 
 use crate::expression::Data;
 
 #[derive(Debug)]
 pub struct Memory {
-    mem: HashMap<i64, Data>,
+    mem: HashMap<i64, Data, BuildHasherDefault<NoHashHasher<i64>>>,
 }
 
 impl Memory {
     pub fn new() -> Memory {
         Memory {
-            mem: HashMap::new()
+            mem: HashMap::with_hasher(BuildHasherDefault::default())
         }
     }
 
@@ -19,6 +22,6 @@ impl Memory {
     }
 
     pub fn get(&self, id: i64) -> &Data {
-        self.mem.get(&id).expect(format!("Memory Error: Could not find variable {}", id).as_str())
+        self.mem.get(&id).unwrap()
     }
 }
