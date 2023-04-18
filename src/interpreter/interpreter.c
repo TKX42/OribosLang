a53077c9d3eb4c0bd6b2044ce754805dee794924
TKX42 <tristan@kaupa.com>
2023-04-18 08:22:37 +0200

    basic const and print instructions

--- src/interpreter/interpreter.c
+++ src/interpreter/interpreter.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
 enum DataType {
     INT,
     FLOAT,
-    STRING
+    STRING,
+    NIL
 };
 
 struct string {
@@ -20,6 +23,10 @@ struct Data {
     } data;
 };
 
+void runtime_error(char *msg) {
+    printf("Runtime Error: %s", msg);
+    exit(1);
+}
 
 void print_str(struct string *string) {
     printf("%s\n", (char *) string->data);
@@ -36,20 +43,64 @@ void print(struct Data *data) {
         case STRING:
             print_str(&data->data.string);
             break;
+        case NIL:
+            runtime_error("Cannot print NIL");
+            break;
+    }
+}
+
+enum instr_type {
+    LOAD,
+    ASSIGN,
+    CONST,
+    PRINT
+};
+
+struct instr {
+    enum instr_type type;
+    struct Data data;
+};
+
+void instr_run(struct instr *instruction, struct Data *stack, int *sc) {
+    switch (instruction->type) {
+        case LOAD:
+            break;
+        case ASSIGN:
+            break;
+        case CONST:
+            stack[*sc] = instruction->data;
+            break;
+        case PRINT:
+            print(&stack[*sc]);
+            sc--;
+            break;
+    }
+}
+
+void vm_run(struct instr *instructions, size_t len, struct Data *stack) {
+    int pc = 0;
+    int sc = 0;
+    while (pc < len) {
+        instr_run(&instructions[pc], stack, &sc);
+        pc++;
     }
 }
 
 int main() {
-    printf("Data Type Test\n");
+    printf("ORIBOS_C\n");
 
-    struct Data d1 = {STRING, {.string="hello there!"}};
-    print(&d1);
+    int STACK_SIZE = 256;
+    struct Data stack[STACK_SIZE];
 
-    struct Data d2 = {INT, {.integer=42}};
-    print(&d2);
+    struct instr instr_1 = {CONST, {INT, {.integer=42}}};
+    //struct instr instr_1 = {CONST, {NIL}};
+    struct instr instr_2 = {PRINT, {NIL}};
+    struct instr instr_3 = {CONST, {STRING, {.string="ez"}}};
+    struct instr instr_4 = {PRINT, {NIL}};
 
-    struct Data d3 = {FLOAT, {.floating=4.2f}};
-    print(&d3);
+    struct instr instructions[] = {instr_1, instr_2, instr_3, instr_4};
+
+    vm_run(&instructions, NELEMS(instructions), stack);
 
     return 0;
 }
